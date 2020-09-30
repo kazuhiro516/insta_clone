9b4b5236a3665539639f33912d32338cc44af481
黒川和弘 <k.kurokawa11@gmail.com>
2020-09-30 22:38:35 +0900

    投稿時間表示の日本語化

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' } 
-  root 'static_pages#home'
+  root 'posts#index'
   get '/users/:id', to: 'users#show', as: 'user'
 
   resources :posts, only: %i(new create index) do
