296f8e7e62266e1400a425558785ee0f49903200
黒川和弘 <k.kurokawa11@gmail.com>
2020-10-05 12:33:07 +0900

    ユーザー一覧画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'posts#index'
   get '/users/:id', to: 'users#show', as: 'user'
+  get '/users', to: 'users#index'
 
   resources :posts, only: %i(new create index show destroy) do
     resources :photos, only: %i(create)
