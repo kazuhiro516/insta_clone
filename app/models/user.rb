bd8ea3feae640a334df3de20bc0bff652f17f609
黒川和弘 <k.kurokawa11@gmail.com>
2020-10-10 17:08:47 +0900

    フォロー機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,31 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes
   has_many :comments
+  has_many :following_relationships, foreign_key: "follower_id", 
+                                      class_name: "Relationship", 
+                                       dependent: :destroy
+  
+  has_many :followings, through: :following_relationships
+
+  has_many :follower_relationships, foreign_key: "following_id", 
+                                     class_name: "Relationship", 
+                                      dependent: :destroy
+
+  has_many :followers, through: :follower_relationships
+
+  def following?(other_user)
+    following_relationships.find_by(following_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    following_relationships.create!(following_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    following_relationships.find_by(following_id: other_user.id).destroy
+  end
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,16 @@ Rails.application.routes.draw do
   get '/users/:id', to: 'users#show', as: 'user'
   get '/users', to: 'users#index'
 
-  resources :posts, only: %i(new create index show destroy) do
-    resources :photos, only: %i(create)
-    resources :likes, only: %i(create destroy)
-    resources :comments, only: %i(create destroy)
+  resources :users do
+    member do
+     get :following, :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
+
+  resources   :posts,   only: %i(new create index show destroy) do
+    resources :photos,  only: %i(create)
+    resources :likes,   only: %i(create destroy)
+    resources :comments,only: %i(create destroy)
   end
 end
