d3fe58c09a9d02fbcdc937edcd74ab3b4fdda7d4
黒川和弘 <k.kurokawa11@gmail.com>
2020-10-24 21:36:52 +0900

    flash機能の調整

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,25 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
 
-  def edit
-    @user = User.find_by(id: params[:id])
-  end
-
-  def update
-    @user = User.find(resource_name)
-    if @user.update(user_params)
-      flash[:success] = "Profile updated"
-      redirect_to @user
-    else
-      flash[:alert] = "更新に失敗しました"
-      render 'edit'
-    end
-  end
-
   protected
-    def user_params
-      params.permit(:name, :email, :password, :authenticity_token,:commit,:_method,
-                                   :password_confirmation, :introduction)
-    end
 
     def update_resource(resource, params)
       resource.update_without_current_password(params)
